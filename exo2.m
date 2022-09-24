894e235249c94a988208555a0d19d70d04405a86
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2022-09-24 22:51:14 +0200

    fin de soiree

---
+++ exo2.m
@@ -0,0 +1,35 @@
+clear variables;
+close all;
+%{
+Programme qui traite de la méthode de monte carlo
+%}
+
+%Fonction anonyme
+mi
+
+question = 1
+switch question;
+  case 1
+    
+    x = linspace(0,1,1000);
+    y = sqrt(1-x.^2)
+    figure(1);
+    scatter(x,y,'x')
+    title("Méthode de Monte-Carlo")
+    
+    mitrailleuse(n)
+    
+    
+    
+    
+    
+    
+  case 2
+    
+    
+    
+    
+    
+    
+    
+end
