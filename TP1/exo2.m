b2e6ca256b987896f5c16f2de3c6058e680f3eb1
Jean-Christophe-BURNOT <jean-christophe.burnot@cpe.fr>
2022-09-25 22:14:57 +0200

    Fin du TP1

--- TP1/exo2.m
+++ TP1/exo2.m
@@ -14,7 +14,7 @@ mitrailleuseORD = @(nb) ones(nb,1)*0.5-(0.5)*rand(nb,1)
 %A vertu a être utilisée avec des scalaires (pas de ./)
 fonction1 = @(ant) log(ant)/(ant^2)
 
-question = 2
+question = 3
 switch question;
   case 1
     x = linspace(0,1,1000);
@@ -37,7 +37,6 @@ switch question;
     de point pour faire mes tirs et mon cercle
     je vais devoir créer les images des antécédants
     utilisées par les tirs pour comparer
-    
     On aurais pu utiliser la norme des points puisque
     on se base sur le point (0,0)
     %}
@@ -107,9 +106,47 @@ switch question;
     disp(text3)
     %===================
     
-    
-    
-    
-    
-    
+  case 3
+    %{
+    La valeur théorique de la double intégrale est approximativement
+    0.06667
+    %}
+    %=====Affichage=====
+    [x,y] = meshgrid(1:.05:5);
+    Courbe = 1./(x+y).^3;
+    figure(1)
+    hold on;
+    mesh(x,y,Courbe)
+    view(-131,22)
+    grid on;
+    xlim([1,3])
+    xlabel('x')
+    ylim([2,5])
+    ylabel('y')
+    zlim([0,0.04])
+    zlabel('$\displaystyle 1/(x+y)^3$','interpreter','latex')
+    %===================
+    %Code appartenant à Axel calculs des points dans
+    %La structure itérative
+    NombrePoint = 10000;
+    NombrePointSousLaCourbe = 0;
+    for i = 1:NombrePoint
+      Point = [1+2*rand(1), 2+2*rand(1), 0.04*rand(1)];
+      if Point(3) < 1/(Point(1)+Point(2)).^3
+        color = [0,1,0];
+        NombrePointSousLaCourbe = NombrePointSousLaCourbe + 1;
+      else
+        color = [1,0,0];
+      end
+            plot3(Point(1), Point(2), Point(3), '*', 'color', color)
+    end
+  ValeurExacteCourbe = 2/75
+  ValeurEmpiriqueCourbe = NombrePointSousLaCourbe/NombrePoint * 2*2*0.04;
+  text1 = sprintf('Nombre total de points : %d', NombrePoint);
+  text2 = sprintf('Nombre de points sous la courbe : %d', NombrePointSousLaCourbe);
+  text3 = sprintf('Valeur approximative de l''int�grale : %f', ValeurEmpiriqueCourbe);
+  disp('VALEUR APPROXIMATIVE D''UNE INTEGRALE')
+  disp(text1)
+  disp(text2)
+  disp(text3) 
 end
